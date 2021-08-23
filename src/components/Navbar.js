19992afc32f72308d03c6cd004a9df35d18c1b51
abriilo <abrilojson@gmail.com>
2021-08-23 18:18:02 +0300

    phase 1 material ui added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {AppBar, Toolbar,Typography, Button} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-class Navbar extends React.Component{
-  render() {
+const useStyles = makeStyles({
+  title: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    padding: '2px 2px 2px 2px'
+  },
+})
+
+const Navbar = () =>{
+  const classes = useStyles()
     return(
-     <nav className='navbar'>
-       <div>
-         <ul>
-           <li><Link to='/'>Home</Link></li>
-           <li><Link to='/about'>About</Link></li>
-           <li><Link to='/contact'>Contact</Link></li>
-         </ul>
-       </div>
-     </nav>
+     <AppBar position="static">
+     <Toolbar>
+       <Typography variant="h6" className={classes.title}>
+         Reactjs-Router
+       </Typography>
+       <Button color="primary"><Link to='/home'>Home</Link></Button>
+       <Button color="inherit"><Link to='/about'>About</Link></Button>
+       <Button color="inherit"><Link to='/contact'>Contact</Link></Button>
+     </Toolbar>
+    </AppBar>
     )
-  }
 }
 
 export default Navbar
